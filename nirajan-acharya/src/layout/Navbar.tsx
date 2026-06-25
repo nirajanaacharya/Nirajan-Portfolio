4cc99534f89d5a237f629ff71924e4006c2fa33a
nirajanaacharya <nirajanlegendkilker15@gmail.com>
2026-06-25 22:39:09 +0545

    updated navbar

--- nirajan-acharya/src/layout/Navbar.tsx
+++ nirajan-acharya/src/layout/Navbar.tsx
@@ -1,16 +1,19 @@
-import {Button} from "../components/Button"
+import {Button} from "../components/Button";
+import {Menu,X} from "lucide-react"
+import {useState} from "react";
 
 const navLinks = [
     {href: "#about",label : "About"},
     {href: "#projects",label : "Project"},
     {href: "#experience",label : "Experience"},
-    {href: "#testimonials",label : "Testimonials"},
-    
+    {href: "#testimonials",label : "Testimonials"}
 
 ]
 
 
 export const Navbar =  () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     return(
         <header className = "fixed top-0 left-0 bg-transparent py-5 right-0 ">
             <nav className = "container mx-auto px-6 flex items-center justify-between ">
@@ -19,7 +22,7 @@ export const Navbar =  () => {
                 className ="text-xl font-bold tracking-tight hover:text-primary" >
                     NA<span className = "text-primary">.</span>
                 </a>
-                <div className ="flex  items-center gap-1">
+                <div className =" hidden md:flex  items-center gap-1">
                     <div className = "glass rounded-full px-2 py-2 py-1 flex items-center gap-1">
                          {navLinks.map((link,index) =>(
                             <a href ={link.href} key ={index}
@@ -27,10 +30,34 @@ export const Navbar =  () => {
                          ))}
                     </div>
                 </div>
-                <div>
-                    <Button>Contact me</Button>
+                <div className = "hidden md:block">
+                    <Button size = "sm">Contact me</Button>
                 </div>
+                {/* mobile menu Button*/}
+                
+                <button className = "md:hidden p-2 text-foreground cursor-pointer "
+                 onClick = {() => setIsMobileMenuOpen((prev) => !prev)}
+                 >
+                   {isMobileMenuOpen? <X size = {24}/>: <Menu size ={24}/>}
+                </button>
             </nav>
+            {/* Mobile Menu */}
+            {isMobileMenuOpen && (
+            <div className="md:hidden glass-strong animate-fade-in">
+                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                {navLinks.map((link, index) => (
+                    <a
+                    href={link.href}
+                    key={index}
+                    className="text-lg text-muted-foreground hover:text-foreground py-2"
+                    >
+                    {link.label}
+                    </a>
+                ))}
+                <Button size="sm">Contact Me</Button>
+                </div>
+            </div>
+            )}
         </header>
     )  
 }
--- nirajan-acharya/src/sections/Hero.tsx
+++ nirajan-acharya/src/sections/Hero.tsx
@@ -1,7 +1,38 @@
 export const Hero =  () => {
     return(
-        <section>
-            {/* <p>I am here...</p> */}
+        <section className = "relative min-h-screen flex items-center overflow-hidden ">
+            <div>
+                <img src="/hero-bg.jpg" alt="portfolio-bg"
+                className = "w-full h-full object-cover opacity-40" 
+                />
+                <div className = "absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background">
+                </div>
+            </div>
+            {/* Greeen Dot */}
+            <div className = "absolute insert-0 overflow-hidden pointer-events-none" >
+                {[...Array(30)].map((_, i)=>(
+                    <div className = "absolute w-1.5 h-1.5 rounded-full opacity-60"
+                    style= {{
+                        backgroundColor : "#20B2A6",
+                        left:`${Math.random() *100}%`,
+                        top : `${Math.random() *100}%`,
+                        animation: `slow-drift ${15 + Math.random()*20}s ease-in-out infinite`,
+                        animationDelay: `${Math.random()*5}s`
+                    }}/>
+                ))}
+            </div>
+
+            {/* Content */}
+            <div>
+                {/* Left Column Text Content */}
+                <div>
+                    <div>
+                        <span>Software Developer</span>
+                    </div>
+                </div>
+                {/* Right Column Image Content */}
+            </div>
+
             
         </section>
     )
