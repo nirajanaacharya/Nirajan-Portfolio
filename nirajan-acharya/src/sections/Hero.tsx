7e1245481eee3e6c2d8c6da2fe775126a6a5cff0
nirajanaacharya <nirajanlegendkilker15@gmail.com>
2026-06-26 22:35:49 +0545

    added animations

--- nirajan-acharya/src/sections/Hero.tsx
+++ nirajan-acharya/src/sections/Hero.tsx
@@ -1,3 +1,7 @@
+import {Button} from "../components/Button"
+import {ArrowRight, Download} from "lucide-react"
+
+
 export const Hero =  () => {
     return(
         <section className = "relative min-h-screen flex items-center overflow-hidden ">
@@ -23,16 +27,49 @@ export const Hero =  () => {
             </div>
 
             {/* Content */}
-            <div>
+        <div className = "container mx-auto px-6 pt-32 pb-20 relative z-10">
+            <div className = "grid lg:grid-cols-2 gap-12 items-center">
                 {/* Left Column Text Content */}
-                <div>
-                    <div>
-                        <span>Software Developer</span>
+                <div className = "space-y-8">
+                    <div className = "animate-fade-in" >
+                        <span className= "inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm text-primary ">
+                            <span className = "w-2 h-2 bg-primary rounded-full "/>
+                            Software Developer</span>
                     </div>
+                    <div className = " space-y-4">
+                        <h1 className = "text-5xl md:text-6xl lg:text-7xl font-bold leading-tight animate-fade-in animation-delay-100 ">
+                            Crafting <span className = "text-primary glow-text">digital</span>
+                        <br />
+                        experience with 
+                        <br />
+                        <span className = "font-serif italic font-normaltext-white">
+                            Precision</span>
+                        </h1>
+                        <p className = "text-lg text-muted-foreground mx-w-lg animate-fade-in animation-delay-300">
+                            Hi, I'm Nirajan Acharya -a software developer specializing in
+                            Java and Web development. I build Scalable, flexible and <i>Awesome </i>
+                             software.
+                        </p>
+                    </div>
+
+
+                    {/* CTAS */}
+
+                    <div>
+                        <Button size = "lg">
+                            Contact Me <ArrowRight className = "w-5 h-5"/>
+                        </Button>
+
+                    </div>
+                    <button className = "">
+                        <span> <Download/>Download Cv  </span>
+                    </button>
+
+
                 </div>
                 {/* Right Column Image Content */}
             </div>
-
+        </div>
             
         </section>
     )
